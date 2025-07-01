611bc9aba259c8dc2842646f34d6855079d7522b
Ruben Campos Delgado <ruben.camposdelgado@enercity.de>
2025-07-01 11:51:55 +0200

    Added correct path for files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from invoice_emailer import InvoiceEmailer
 
 if __name__ == "__main__":
-    emailer = InvoiceEmailer('your_file.xlsx', 'your_file_with_emails.xlsx')
+    emailer = InvoiceEmailer('Y:/BaswareReminder/InvoiceEmailer/test_data/your_file.xlsx', 'Y:/Baswarereminder/InvoiceEmailer/results/your_file_with_emails.xlsx')
     emailer.load_data()
     emailer.dict_emails_oe()
     emailer.add_emails_to_basware()
