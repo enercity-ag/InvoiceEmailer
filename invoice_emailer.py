ce51e0693b27e0e056cea8855706b50d9607652f
Ruben Campos Delgado <ruben.camposdelgado@enercity.de>
2025-07-09 12:44:20 +0200

    OE Spalte automatisch erstellen

--- invoice_emailer.py
+++ invoice_emailer.py
@@ -1,127 +1,43 @@
 import pandas as pd # For reading and manipulating excel files
-import collections # used here for defaultdict to group messages by email
-import win32com.client as win32 # Allows interaction wih Micorosoft Oulook to send emails.
+
+# Create a path to the files in OneDrive. Use your own ID.
+BASWARE_USERS_PATH = r"C:\Users\uac7200\OneDrive - enercity\basware Rechnungsbearbeitung - Technische Umsetzung - Dokumente\Technische Umsetzung\Rohdaten\basware\User-Stammdaten\User Rights Report.xlsx"
+BASWARE_CURRENT_REPORT = r"C:\Users\uac7200\OneDrive - enercity\basware Rechnungsbearbeitung - Technische Umsetzung - Dokumente\Technische Umsetzung\Rohdaten\basware\Rechnungen (basware_080725).xlsx"
+WORKDAY_USERS_PATH = r"C:\Users\uac7200\OneDrive - enercity\basware Rechnungsbearbeitung - Technische Umsetzung - Dokumente\Technische Umsetzung\Rohdaten\workday\Personalsuchliste_für_F_(RW)_monatlich.xlsx"
+
+# Read the excel files
+df_basware_users = pd.read_excel(BASWARE_USERS_PATH, sheet_name="User Rights")
+df_basware_current_report = pd.read_excel(BASWARE_CURRENT_REPORT, sheet_name="Rechnungen")
+df_workday_users = pd.read_excel(WORKDAY_USERS_PATH, sheet_name="Sheet1", header=1)
+
+# Print the first entry of the files
+print(df_basware_users.head())
+print(df_basware_current_report.head())
+print(df_workday_users.head())
+
+# First step: add a Username column in df_basware_current_report from df_basware_users, by matching Empänger in df_current_report 
+# with User in df_basware_users.
+merged_step1 = df_basware_current_report.merge(
+    df_basware_users[['User', 'Username']],
+    left_on = 'Empfänger',
+    right_on = 'User',
+    how = 'left'
+)
+
+# Second step: add a OE column in the previously df_basware_current_report from df_workday_users, by matching username in merged_step1
+# with UserID in df_workday_users.
+df_final = merged_step2 = merged_step1.merge(
+    df_workday_users[['UserID', 'OE']],
+    left_on = 'Username',
+    right_on = 'UserID',
+    how = 'left'
+)
+
+# Show the result
+print(df_final.head())
+df_final.to_excel("Verknüpfung_basware_workday.xlsx")
 
 
-class InvoiceEmailer:
-
-    def __init__(self, input_file, output_file, domain = 'enercity.de'):
-        self.input_file = input_file
-        self.output_file = output_file
-        self.domain = domain
-        self.sheet_basware = None
-        self.sheet_workday = None
-        self.email_dict = {}
-   
-    def load_data(self):
-        """ Read the excel file, which consists of two sheets,
-        one for basware, one for workday.
-        """
-        self.sheet_basware = pd.read_excel(self.input_file, sheet_name = 0)
-        self.sheet_workday = pd.read_excel(self.input_file, sheet_name = 1)
- 
-    def extract_prefix(self, oe):
-        """ Extract the prefix before the dash. For example, F-RN returns F.
-        This identifies the person(s) in charge for the given department.
-        """
-        if pd.isna(oe):
-            return ''
-        return str(oe).split('-')[0]
-   
-    def format_email(self, name):
-        """ Takes Surname Name and returns the corresponding email address name.surname@domain
-        """
-        if pd.isna(name) or not isinstance(name, str):
-            return ''
-        parts = name.strip().lower().split()
-        if len(parts) < 2:
-            return ''
-        first, last = parts[-1], parts[0]
-        return f"{first}.{last}@{self.domain}"
-   
-    def dict_emails_oe(self):
-        """ Dictionary between prefixes and emails. """
-        for _, row in self.sheet_workday.iterrows():
-            oe_prefix =str(row.get('OE','')).strip()
-            if '-' not in oe_prefix and oe_prefix.isalpha(): # Only accept OEs without dashes and with letters
-                name = row.get('Name Gesamt', '')
-                email = self.format_email(name)
-                if email:
-                    self.email_dict.setdefault(oe_prefix, []).append(email)
-
-    def add_emails_to_basware(self):
-        """ Adds an additional column in the basware file, which contain the email(s)
-        of the person(s) in charge of the department. """
-        self.sheet_basware['OE_prefix'] = self.sheet_basware['OE'].apply(self.extract_prefix)
-        self.sheet_basware['Emails'] = self.sheet_basware['OE_prefix'].apply(lambda prefix: '; '.join(self.email_dict.get(prefix, [])))
-       
-    def save_new_file(self):
-        """ Create a separate basware file with the emails column
-        """
-        self.sheet_basware.to_excel(self.output_file, index = False)
-        print(f"Updated file saved as: {self.output_file}")
-
-    def send_emails(self):
-        """ Send emails to the people in charge (Vorgesetzten) with a customized message. """
-        # Create Outlook interface
-        outlook = win32.Dispatch('outlook.application')
-
-        # Read the sheet of the new file
-        self.sheet_basware = pd.read_excel(self.output_file, sheet_name = 0)
-
-        # Dictionary to collect messages per OE group
-        oe_messages = collections.defaultdict(list)
-
-        # Read emails, invoice numbers and delate days
-        for _, row in self.sheet_basware.iterrows():
-            oe_prefix = row.get('OE_prefix', '')
-            invoice_number = row.get('Rechnungsnummer', '')
-            days_due = row.get('Ausstehend seit', '')
-            supplier = row.get('Lieferantencode', '')
-
-            # Skip invalid rows
-            if pd.isna(oe_prefix) or pd.isna(invoice_number) or pd.isna(days_due):
-                 continue
-           
-            message = f"Die Rechnung {invoice_number} mit Lieferantencode {supplier} ist seit {days_due} Tagen ausstehend. "
-            oe_messages[oe_prefix].append(message)
-           
-        # Send one email per OE group. Modify the introductory message as you wish
-        for oe_prefix, messages in oe_messages.items():
-            recipients = self.email_dict.get(oe_prefix, [])
-            if not recipients:
-                continue
-           
-            recipient_str = ';'.join(recipients)
-            subject = "Quartalsabschluss // Bearbeitung ausstehender Aufgaben im Basware"
-            body =  (
-            "Liebe Kolleg*innen,\n\n"
-            "im Sinne eines schnelleren Prozessdurchlaufes und der stringenteren Einhaltung der Abschluss-Terminpläne, "
-            "widmen wir dem Thema der offenen Aufgaben bei Eingangsrechnungen-Bearbeitungen derzeit bekanntlich mehr Augenmerk.\n"
-            "Bitte die folgenden offenen Basware - Aufgaben für den Verantwortungsbereich beachten.\n\n"
-            )
-            body += '\n'.join(messages)
-            body +=  (
-            "\n\nAufgrund der bislang sehr manuellen Arbeiten sind wir derzeit noch in der Fortentwicklung"
-            "einer automatisierten Auswertung und Zuordnung. Daher steht die Namens-/Bereichszuordnung noch unter Vorbehalt."
-            "Sollten euch fehlerhafte Zuordnungen auffallen oder ihr habt weiterbringende Hinweise, meldet euch gerne."
-            "Vielen Dank für eure Unterstützung, Bearbeitung und Koordination.\n"
-            "Positive Grüße\n\n Das Team Rechnungswesen Nebenbücher"
-            )
-
-            # Create email. One for each recipient
-            mail = outlook.CreateItem(0)
-            mail.To = recipient_str
-            mail.CC = "susanne.urban@enercity.de" #Add CC recipients
-            mail.Subject = subject
-            mail.Body = body
-            mail.Display() # Check before sending
-            # mail.send() # Finally send
-
-
-
-
-    
 
 
 
